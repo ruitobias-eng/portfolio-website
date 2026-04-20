0b242986807dcfb86d312ecc174f665f959f5c09
Rui Tobias Carvalho <bibolook@gmail.com>
2026-04-20 20:28:30 -0300

    Change base path for portfolio website

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/', // raiz do domínio para Custom Domain
+  base: '/portfolio-website/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
